Fixed bug in variance
Former-commit-id: 4cf0bacb0e618c409dade859f56145938b277c50

paper_plots_borg_antihalos.py
@@ -500,7 +500,7 @@ if doPPTs:
         show=True,rBins=rBins,clusterNames=clusterNames,rescale=False,\
         density=False,legLoc = [0.3,0.1],hspace=0.3,\
         ylabel='Number of galaxies $ < r$',height=0.7,fontsize=8,\
-        showPoissonRange=False,color2='grey')
+        showPoissonRange=False,color2='grey',showVariance=False)
     plot.plotPPTProfiles(np.sum(galaxyNumberCountExp,2),\
         np.sum(galaxyNumberCountsRobustAll,3),\
         savename=figuresFolder + "ppt_Ngal_poisson" + suffix + ".pdf",\